19935be4303f185727aa75f93802027bbe09beea
Faggioni <miguelfaggioni@gmail.com>
2015-11-02 22:36:34 -0430

    All First Six Assignments Completed
    only two more test to go

--- 5th Week/Matlab/Programming Assignment/movies.m
+++ 5th Week/Matlab/Programming Assignment/movies.m
@@ -1,21 +1,26 @@
-function [seemovies] = movies(h1,h2,m1,m2,d1,d2)
+function [seemovies] = movies(h1,m1,d1,h2,m2,d2)
 %movies Summary of this function goes here
 %   Detailed explanation goes here
-
-    ht1=0;
-    mt2=0;
     
+    minabsp1 = 0;
+    minabsp1 = ((h1-1) * 60) + m1;
     
-
-
-if se solapan no la ven
-
-elseif si no se solapan pero comienzan despues de la espera de 30 min
-
+    minabsp2 = 0;
+    minabsp2 = ((h2-1) * 60) + m2;
+    seemovies=true;
     
-else if  h2>=12 && m2>0
+    %SOLAPAMIENTO
+    if (minabsp1 + d1 > minabsp2)
         seemovies=false;
+        %PELICULA DESPUES DE LAS 12
+        elseif  h2>=12 && m2>0
+                seemovies=false;
+                %ESPERA MAS DE 30 MINUTOS
+                elseif (minabsp1 + d1 + 30) < minabsp2
+                    seemovies = false;
     else
-        seemovies=true;
-end
+        seemovies = true
+    end
+    
+ end
 
