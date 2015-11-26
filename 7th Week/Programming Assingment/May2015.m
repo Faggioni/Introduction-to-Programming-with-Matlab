656cf69566130596741af7420bcb25034e0f0606
Faggioni <miguelfaggioni@gmail.com>
2015-11-26 08:33:06 -0430

    7th Week / First two items completed

--- 7th Week/Programming Assingment/May2015.m
+++ 7th Week/Programming Assingment/May2015.m
@@ -2,12 +2,29 @@ function [m] = May2015()
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
     
-    m = [1:31];
+    %m = [1:31];
     
-    for i=1:1
-        m(i)=m.date(1);
-        m(i)=m.month('May');
-        m(i)=m.day('Friday');
+    for i=1:31
+        
+        m(i).month = 'May';
+        m(i).date = i;
+        if mod(i-1,7) == 0    
+           m(i).day = 'Fri';
+        elseif mod(i-1,7) == 1
+                m(i).day = 'Sat';
+            elseif mod(i-1,7) == 2
+                m(i).day = 'Sun';
+                    elseif mod(i-1,7) == 3
+                        m(i).day = 'Mon';
+                        elseif mod(i-1,7) == 4
+                           m(i).day = 'Tue';
+                           elseif mod(i-1,7) == 5
+                               m(i).day = 'Wed';
+                                elseif mod(i-1,7) == 6
+                                    m(i).day = 'Thu';
+        end
+        
+   
     end
     
 end
