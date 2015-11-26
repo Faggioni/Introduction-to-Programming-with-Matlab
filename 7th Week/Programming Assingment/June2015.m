7d48370b7e15f87e62a442ce6a658ad48a6b2285
Faggioni <miguelfaggioni@gmail.com>
2015-11-26 09:07:22 -0430

    7th Week / 4th item

--- 7th Week/Programming Assingment/June2015.m
+++ 7th Week/Programming Assingment/June2015.m
@@ -1,7 +1,38 @@
-function [ output_args ] = June2015( input_args )
-%UNTITLED Summary of this function goes here
+function [cell] = June2015()
+%June2015 Summary of this function goes here
 %   Detailed explanation goes here
-
+    
+for i=1:30
+        for j=1:3
+        
+            if j==1
+                cell{i,j} = 'June';
+            end
+            
+            if j==2
+                cell{i,j} = i;
+            end
+            
+            if j==3
+                if mod(i-1,7) == 0    
+                   cell{i,j} = 'Mon';
+                elseif mod(i-1,7) == 1
+                        cell{i,j} = 'Tue';
+                    elseif mod(i-1,7) == 2
+                        cell{i,j} = 'Wed';
+                            elseif mod(i-1,7) == 3
+                                cell{i,j} = 'Thu';
+                                elseif mod(i-1,7) == 4
+                                   cell{i,j} = 'Fri';
+                                   elseif mod(i-1,7) == 5
+                                       cell{i,j} = 'Sat';
+                                        elseif mod(i-1,7) == 6
+                                            cell{i,j} = 'Sun';
+        end
+        
+        end
+    end
+    
 
 end
 
---
+++ 7th Week/Programming Assingment/codeit.m
@@ -0,0 +1,7 @@
+function [coded] = codeit(string)
+%codeit Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
