759774e324abe3056dc667f622c50ea64722e5f7
Faggioni <miguelfaggioni@gmail.com>
2015-11-27 19:02:30 -0430

    Parenthesis

--- 7th Week/Programming Assingment/censor.m
+++ 7th Week/Programming Assingment/censor.m
@@ -4,15 +4,28 @@ function [cellout] = censor(cellint,string)
         cellout=cell(0);
         j=1;
         i=1;        
-        
-        while (j<=length(cellint))
-             if isempty((strfind(cellint{1,j},string)))
-             cellout{1,i}=cellint{1,j};
-             i=i+1;
-             end
+        [r c] = size(cellint)
+        %VECTOR FILA
+        if r == 1
+            while (j<=c)
+                 if isempty((strfind(cellint{1,j},string)))
+                 cellout{1,i}=cellint{1,j};
+                 i=i+1;
+                 end
+                 j=j+1;
+
+            end
+        end
+        %VECTOR COLUMNA
+        if c==1
+             while (j<=r)
+                 if isempty((strfind(cellint{j,1},string)))
+                 cellout{i,1}=cellint{j,1};
+                 i=i+1;
+                 end
              j=j+1;
         
+            end
         end
-        
 end
 
